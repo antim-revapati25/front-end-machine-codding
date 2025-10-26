b9fc4e372c61ae9bc19235400f9fe14c1717f897
antim-revapati25 <antim.revapati25@gmail.com>
2025-10-26 20:39:32 +0530

    Create Home.jsx

---
+++ 3_pagination/Home.jsx
@@ -0,0 +1,12 @@
+import CardLayout from "./components/CardLayout";
+import "./css/index.css";
+const Home = () => {
+  return (
+    <div className="general">
+      <h1>Pagination</h1>
+      <CardLayout />
+    </div>
+  );
+};
+
+export default Home;
